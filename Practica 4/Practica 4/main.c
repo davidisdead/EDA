b7d673a6933b9cdf651af7d8689496de97c0e35b
David Hernandez <d_h_h_03@hotmail.com>
2025-10-17 12:25:23 -0600

    Fix login bug

---
+++ Practica 4/Practica 4/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Practica 4
+//
+//  Created by David Hernández on 17/10/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
