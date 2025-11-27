69f67d8abbe30a3faaa41618ab79de53af136962
David Hernandez <d_h_h_03@hotmail.com>
2025-11-27 00:22:30 -0600

    Subida de archivos

---
+++ Practica 8/Practica 8/lista_doble.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Practica 8
+//
+//  Created by David Hernández on 27/11/25.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
